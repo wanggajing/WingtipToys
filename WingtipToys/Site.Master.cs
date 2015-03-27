e5870dff999ce05b6b0ef81a2d9e2bfe51f72f08
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-27 16:51:59 +1300

    Start the project all over again

--- WingtipToys/Site.Master.cs
+++ WingtipToys/Site.Master.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Security.Principal;
 using System.Web;
 using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
@@ -69,6 +71,12 @@ namespace WingtipToys
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new WingtipToys.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
         }
 
         protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
