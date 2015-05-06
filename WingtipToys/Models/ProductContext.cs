ea0573d0b31a9464e74ac9aa3fe5eb8cf61bd42e
Wu Ke <wke918@aucklanduni.ac.nz>
2015-05-06 17:35:18 +1200

    Building shoppingcart

--- WingtipToys/Models/ProductContext.cs
+++ WingtipToys/Models/ProductContext.cs
@@ -12,6 +12,7 @@ namespace WingtipToys.Models
         { 
         } 
         public DbSet<Category> Categories { get; set; } 
-        public DbSet<Product> Products { get; set; } 
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
     }
 }
